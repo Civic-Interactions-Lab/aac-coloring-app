678733540c41c4615c9edea7c12754317771486f
parth099 <parthnpatel715@gmail.com>
2023-08-21 11:59:51 -0400

    fixed singleton instance loss error

--- backend/openai_transcribe.js
+++ backend/openai_transcribe.js
@@ -1,25 +1,16 @@
 const { OpenAI } = require("openai");
 const fs = require("fs");
 
-class OpenAiFactory {
-    constructor(apiKey) {
-        if (OpenAiFactory._instance) {
-            return OpenAiFactory._instance;
-        }
-        OpenAiFactory._instance = this;
-        return new OpenAI({
-            apiKey,
-        });
-    }
-}
-
 async function transcribe(openai_key, filename) {
-    const openai = new OpenAiFactory(openai_key);
+    const openai = new OpenAI({
+        apiKey: openai_key,
+    });
     const transcription = await openai.audio.transcriptions.create({
         file: fs.createReadStream(filename),
         model: "whisper-1",
     });
 
+    console.log(transcription);
     return transcription;
 }
 
