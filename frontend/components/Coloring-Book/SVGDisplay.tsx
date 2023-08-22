b2587d341135bec9994f85ec6df0bbf7a555db95
parth099 <parthnpatel715@gmail.com>
2023-08-22 13:41:14 -0400

    migration successful to SVGLoader from ReactSVG

--- frontend/components/Coloring-Book/SVGDisplay.tsx
+++ frontend/components/Coloring-Book/SVGDisplay.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { getS3ObjectByKey } from "@/util/s3Util";
 import React from "react";
+import { SvgLoader, SvgProxy } from "react-svgmt";
 import { ReactSVG } from "react-svg";
 import SmallLoadingSpinner from "../general/SmallLoadingSpinner";
 import { useAudioTranscriptionContext } from "@/contexts/audioTranscriptionContext/audioTranscriptionContext";
@@ -12,24 +13,15 @@ export interface SVGDisplayProps {
 export default function SVGDisplay(props: SVGDisplayProps) {
     const { SVGPaintActions } = useAudioTranscriptionContext();
 
-    const css = SVGPaintActions.map((SVGPaintAction) => `#${SVGPaintAction.object}{fill: ${SVGPaintAction.color};}`).join("\n");
-    console.log(css);
-
     if (!props.imageId) return null;
 
     return (
         <div className="flex flex-col w-full justify-center items-center" id="SVG-top-level-container">
-            <style jsx>{css}</style>
-            <ReactSVG
-                src={getS3ObjectByKey(props.imageId)}
-                httpRequestWithCredentials={false}
-                loading={SmallLoadingSpinner}
-                className="max-h-screen w-full"
-                beforeInjection={(svg) => {
-                    svg.setAttribute("width", "");
-                    svg.setAttribute("height", "");
-                }}
-            />
+            <SvgLoader path={getS3ObjectByKey(props.imageId)} width="100%" height="100%">
+                {SVGPaintActions.map((SVGPaintAction, idx) => (
+                    <SvgProxy key={idx} selector={`#${SVGPaintAction.object}`} fill={SVGPaintAction.color} />
+                ))}
+            </SvgLoader>
         </div>
     );
 }
