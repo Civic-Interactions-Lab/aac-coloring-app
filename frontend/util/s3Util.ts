20ba6ec72bb77c3965cfebb8e9fe05873b9806a9
Parth Patel <33704202+Parth099@users.noreply.github.com>
2024-03-28 10:02:44 -0400

    todo: implement cache busting for chrome

--- frontend/util/s3Util.ts
+++ frontend/util/s3Util.ts
@@ -1,3 +1,4 @@
+// TODO: implement https://stackoverflow.com/questions/1077041/refresh-image-with-a-new-one-at-the-same-url/22429796#22429796
 export function getS3ObjectByKey(key: string) {
     return `${process.env.NEXT_PUBLIC_S3_URL}/${key}`;
 }
